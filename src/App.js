651b5d07d0ecf50415358309865cea7f12afd85a
rita <kajirita2002@gmail.com>
2020-09-28 01:06:36 +0900

    state追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: {},
+      open: false
+    }
+  }
+
   render() {
     return (
       <div>
